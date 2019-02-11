170ed0061f414f5abc7a6e4b6f14c4d527011181
Nick Fox-Gieg <nick@fox-gieg.com>
2019-02-11 16:45:11 -0500

    add jpeg encoder

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 var strokeLifetime = 10000;
 
 var express = require("express");
-var osc = require('node-osc');
+var osc = require("node-osc");
+var jpeg = require("jpeg-js")
 var app = express();
 var http = require("http").Server(app);
 var port = 8080;
@@ -55,6 +56,32 @@ io.sockets.on('connection', function (socket) {
 });
 
 // ~ ~ ~ ~
+
+// https://www.npmjs.com/package/jpeg-js
+
+function decodeJpeg(img, useArray) { // Buffer or Uint8Array, bool
+    var jpegData = fs.readFileSync(img);
+    var rawImageData = jpeg.decode(jpegData, useArray); // true for Uint8Array
+    return rawImageData;
+}
+
+function encodeJpeg(width, height, compression) { // int, int, int 1-100
+    var frameData = new Buffer(width * height * 4);
+    var i = 0;
+    while (i < frameData.length) {
+      frameData[i++] = 0xFF; // red
+      frameData[i++] = 0x00; // green
+      frameData[i++] = 0x00; // blue
+      frameData[i++] = 0xFF; // alpha - ignored in JPEGs
+    }
+    var rawImageData = {
+      data: frameData,
+      width: width,
+      height: height
+    };
+    var jpegImageData = jpeg.encode(rawImageData, compression);
+    return jpegImageData;
+}
 
 class Frame {
 	constructor() {
